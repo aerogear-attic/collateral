ea3cbde754c8ed651fe501ec12b4ef44f8c3c039
Douglas Campos <qmx@qmx.me>
2012-03-09 15:56:10 -0300

    adding initial archetype sync helper script

---
+++ scripts/sync_archetype.sh
@@ -0,0 +1,24 @@
+#!/bin/sh
+ARCHETYPE_PATH="/Volumes/v3/jboss/as-archetypes/jboss-html5-mobile-archetype/"
+QUICKSTART_PATH="/Volumes/v3/jboss/as-quickstarts/kitchensink-html5-mobile/"
+TMPDIR="$HOME/jboss-tmp"
+
+mkdir -p $TMPDIR
+
+cd $ARCHETYPE_PATH
+mvn install
+cd $TMPDIR
+mvn archetype:generate -DarchetypeGroupId="org.jboss.aerogear.archetypes" \
+-DarchetypeArtifactId="jboss-html5-mobile-archetype" \
+-DarchetypeVersion="1.0.0.M2b-SNAPSHOT" \
+-DgroupId="org.jboss.as.quickstarts" \
+-DartifactId="jboss-as-kitchensink-html5-mobile" \
+-Dversion="1.0.0-SNAPSHOT" \
+-Dpackage="org.jboss.as.quickstarts.html5_mobile"
+cd $TMPDIR
+cd jboss-as-kitchensink-html5-mobile
+git init && git add . && git commit -m "from archetype"
+
+
+
+
